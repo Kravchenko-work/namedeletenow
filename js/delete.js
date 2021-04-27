674631f0fe2cbf0f2993ca8dbc73c7450229e66e
Kravchenko-work <68537638+Kravchenko-work@users.noreply.github.com>
2021-04-27 11:40:57 +0400

    Delete delete.js

--- js/delete.js
+++
@@ -1,5 +0,0 @@
-// Неудавшийся датапикер
-form__date.addEventListener('input', function(e){
-	let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,2})(\d{0,4})/);	
-	e.target.value = x[1] ? (x[2] ? (x[3] ? (x[1] + "  /  " + x[2] + "  /  " + x[3]) : x[1] + "  /  " + x[2]) : x[1]) : "__  / __ / ____";
-});
